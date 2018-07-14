a0a3fadff779eefecbc8d1cda9b2e78b35ad8d24
Nick Antourakis <35820875+nantourakis@users.noreply.github.com>
2018-07-14 00:18:06 -0400

    twitter keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key:         'kqQKPNzuTFEUQWrX6dtslmYc4',
+    consumer_secret:      'uGjRkXq7dwGIpCOBCwdce4IoQoIwf3brPY1Ar2f2qE0r6qVAMn',
+    access_token:         '1016527609566547968-dPrn0h6bRlE857qi6TIRY8yvicitVi',
+    access_token_secret:  'bLMN054gg7WBvOJh4tYDYRiVs2KUZ9AFMeUphnLEuTggC',
+};
+
+
+
